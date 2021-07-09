e21ae7e0f9b480660ffe711f32ef39ca56d24ede
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-07-09 21:51:04 +0300

    done 1

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function flatten(list) {
-  // code here
+  let newArr = list;
+  console.log(newArr.some(Array.isArray));
+  while (newArr.some(Array.isArray)) {
+    const arr = [];
+    newArr.forEach((item, index, array) => {
+      if (Array.isArray(item)) {
+        arr.push(...item);
+      } else {
+        arr.push(item);
+      }
+    });
+    newArr = arr;
+  }
+  console.log(newArr);
 }
 
 flatten([
@@ -12,3 +25,4 @@ flatten([
   { a: 1 },
 ]);
 // возвращает
+//      [1, 'any [complex] string', null, function() {}, 1, 2, 3, '4', 0, { a: 1 }]
