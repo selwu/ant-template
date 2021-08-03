e0f8b7836d7cb5ad0c1ce4b298bddefb68dd5f00
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-08-03 20:16:29 +0300

    ok

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const mobileRemote = (text) => {
   const graph = {};
-  const one = ['1'];
-  const two = ['2', 'a', 'b', 'c'];
-  const three = ['3', 'd', 'e', 'f'];
-  const four = ['4', 'g', 'h', 'i'];
-  const five = ['5', 'j', 'k', 'l'];
-  const six = ['6', 'm', 'n', 'o'];
-  const seven = ['7', 'p', 'q', 'r', 's'];
-  const eight = ['8', 't', 'u', 'v'];
-  const nine = ['9 ', 'w', 'x', 'y', 'z'];
-  const ten = ['*'];
-  const eleven = ['0'];
-  const twelve = ['#'];
+  const zero = ['1'];
+  const one = ['2', 'a', 'b', 'c'];
+  const two = ['3', 'd', 'e', 'f'];
+  const three = ['4', 'g', 'h', 'i'];
+  const four = ['5', 'j', 'k', 'l'];
+  const five = ['6', 'm', 'n', 'o'];
+  const six = ['7', 'p', 'q', 'r', 's'];
+  const seven = ['8', 't', 'u', 'v'];
+  const eight = ['9 ', 'w', 'x', 'y', 'z'];
+  const nine = ['*'];
+  const ten = ['0'];
+  const eleven = ['#'];
 
   const arrButtons = [
+    zero,
     one,
     two,
     three,
@@ -25,25 +26,24 @@ const mobileRemote = (text) => {
     nine,
     ten,
     eleven,
-    twelve,
   ];
 
   const matrix = [
-    [0, two, three, four, 0, 0, 0, 0, 0, ten, 0, 0],
-    [one, 0, three, 0, five, 0, 0, 0, 0, 0, eleven, 0],
-    [one, two, 0, 0, 0, six, 0, 0, 0, 0, 0, twelve],
-    [one, 0, 0, 0, five, six, seven, 0, 0, 0, 0, 0],
-    [0, two, 0, four, 0, six, 0, eight, 0, 0, 0, 0],
-    [0, 0, three, four, five, 0, 0, 0, nine, 0, 0, 0],
-    [0, 0, 0, four, 0, 0, 0, eight, nine, ten, 0, 0],
-    [0, 0, 0, 0, five, 0, seven, 0, nine, 0, eleven, 0],
-    [0, 0, 0, 0, 0, six, seven, eight, 0, 0, 0, twelve],
-    [one, 0, 0, 0, 0, 0, seven, 0, 0, 0, eleven, twelve],
-    [0, two, 0, 0, 0, 0, 0, eight, 0, ten, 0, twelve],
-    [0, 0, three, 0, 0, 0, 0, 0, nine, ten, eleven, 0],
+    [0, one, two, three, 0, 0, 0, 0, 0, nine, 0, 0],
+    [zero, 0, two, 0, four, 0, 0, 0, 0, 0, ten, 0],
+    [zero, one, 0, 0, 0, five, 0, 0, 0, 0, 0, eleven],
+    [zero, 0, 0, 0, four, five, six, 0, 0, 0, 0, 0],
+    [0, one, 0, three, 0, five, 0, seven, 0, 0, 0, 0],
+    [0, 0, two, three, four, 0, 0, 0, eight, 0, 0, 0],
+    [0, 0, 0, three, 0, 0, 0, seven, eight, nine, 0, 0],
+    [0, 0, 0, 0, four, 0, six, 0, eight, 0, ten, 0],
+    [0, 0, 0, 0, 0, five, six, seven, 0, 0, 0, eleven],
+    [zero, 0, 0, 0, 0, 0, six, 0, 0, 0, ten, eleven],
+    [0, one, 0, 0, 0, 0, 0, seven, 0, nine, 0, eleven],
+    [0, 0, two, 0, 0, 0, 0, 0, eight, nine, ten, 0],
   ];
 
-  function search(button) {
+  function searchOne(button) {
     const result = [];
     matrix[button].forEach((item, index) => {
       if (item) {
@@ -53,86 +53,121 @@ const mobileRemote = (text) => {
     return result;
   }
 
+  function searchTwo(button) {
+    const repeat = [];
+    let indexID = [];
+    const result = [];
+    matrix[button].forEach((item, indexOne) => {
+      if (item) {
+        // console.log('indexOne: ', indexOne);
+
+        matrix[indexOne].forEach((item, index) => {
+          if (item) {
+            console.log(index, indexOne);
+
+            indexID.push(index);
+          }
+        });
+      }
+    });
+    indexID = [...new Set(indexID)];
+    console.log('indexID: ', indexID);
+
+    return indexID.map((item) => {
+      return arrButtons[item];
+    });
+  }
+
+  console.log('searchTwo: ', searchTwo(4));
+  console.log('searchOne: ', searchOne(4));
+
   function searchRepeat(first, second) {
     return arrButtons.filter((item) => {
       return !second.includes(item) && item !== first;
     });
   }
 
+  graph.zero = {
+    1: [zero],
+    2: [...searchOne(0)],
+    3: [...searchRepeat(zero, searchOne(0))],
+    4: [],
+  };
   graph.one = {
     1: [one],
-    2: [two, three, four, ten],
-    3: [...searchRepeat(one, search(0))],
+    2: [...searchOne(1)],
+    3: [...searchRepeat(one, searchOne(1))],
   };
   graph.two = {
     1: [two],
-    2: [one, three, four, eleven],
-    3: [...searchRepeat(two, search(1))],
+    2: [...searchOne(2)],
+    3: [...searchRepeat(two, searchOne(2))],
   };
   graph.three = {
     1: [three],
-    2: [search(2)],
-    3: [...searchRepeat(three, search(2))],
+    2: [...searchOne(3)],
+    3: [...searchRepeat(three, searchOne(3))],
   };
   graph.four = {
     1: [four],
-    2: [search(3)],
-    3: [...searchRepeat(four, search(3))],
+    2: [...searchOne(4)],
+    3: [...searchRepeat(four, searchOne(4))],
   };
   graph.five = {
     1: [five],
-    2: [search(4)],
-    3: [...searchRepeat(five, search(4))],
+    2: [...searchOne(5)],
+    3: [...searchRepeat(five, searchOne(5))],
   };
   graph.six = {
     1: [six],
-    2: [search(5)],
-    3: [...searchRepeat(six, search(5))],
+    2: [...searchOne(6)],
+    3: [...searchRepeat(six, searchOne(6))],
   };
   graph.seven = {
     1: [seven],
-    2: [search(6)],
-    3: [...searchRepeat(seven, search(6))],
+    2: [...searchOne(7)],
+    3: [...searchRepeat(seven, searchOne(7))],
   };
   graph.eight = {
     1: [eight],
-    2: [search(7)],
-    3: [...searchRepeat(eight, search(7))],
+    2: [...searchOne(8)],
+    3: [...searchRepeat(eight, searchOne(8))],
   };
   graph.nine = {
     1: [nine],
-    2: [search(8)],
-    3: [...searchRepeat(nine, search(8))],
+    2: [...searchOne(9)],
+    3: [...searchRepeat(nine, searchOne(9))],
   };
   graph.ten = {
     1: [ten],
-    2: [search(9)],
-    3: [...searchRepeat(ten, search(9))],
+    2: [...searchOne(10)],
+    3: [...searchRepeat(ten, searchOne(10))],
   };
   graph.eleven = {
     1: [eleven],
-    2: [search(10)],
-    3: [...searchRepeat(eleven, search(10))],
-  };
-  graph.twelve = {
-    1: [twelve],
-    2: [search(11)],
-    3: [...searchRepeat(twelve, search(11))],
+    2: [...searchOne(11)],
+    3: [...searchRepeat(eleven, searchOne(11))],
   };
 
-  let cash = graph.one;
+  let cash = graph.zero;
 
   const searchSymbol = (symbol) => {
     let resultItem = 0;
-    Object.keys(cash).forEach((item, indexOne) => {
-      cash[item].forEach((item, indexTwo) => {
-        item.forEach((ItemSymbol, indexThree) => {
-          if (symbol === ItemSymbol) {
-            resultItem += indexOne + indexThree + 2;
+    Object.keys(cash).forEach((itemOne, indexOne) => {
+      cash[itemOne].forEach((itemTwo, indexTwo) => {
+        itemTwo.forEach((ItemSymbol, indexThree) => {
+          if (symbol === ItemSymbol && indexOne === 0) {
+            resultItem = indexThree + 2;
+          } else if (symbol === ItemSymbol && indexOne === 1) {
+            resultItem = indexThree + 3;
+          } else if (symbol === ItemSymbol && indexOne === 2) {
+            resultItem = indexThree + 4;
+          } else if (symbol === ItemSymbol && indexOne === 3) {
+            resultItem = indexThree + 5;
           }
         });
-        console.log('itemCash: ', item);
-        console.log('Text: ', indexTwo);
+        // console.log('itemTwo: ', itemTwo);
+        // console.log('Text: ', indexTwo);
       });
     });
 
@@ -142,5 +177,5 @@ const mobileRemote = (text) => {
   return searchSymbol(text);
 };
 
-console.log(mobileRemote('1')); // 6
+console.log(mobileRemote('y')); // 6
 // console.log(mobileRemote('yandex')); // 34
