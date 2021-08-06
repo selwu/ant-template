294e7db0a33687582a7620f42a7d57410c8e39d1
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-08-06 23:19:09 +0300

    fix bold

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const context = canvas.getContext('2d');
 context.strokeStyle = '#fff';
 
 context.beginPath();
-context.moveTo(128, 136);
-context.lineTo(128, 286);
+context.moveTo(120, 144);
+context.lineTo(120, 294);
 context.lineWidth = 16;
 context.stroke();
 
@@ -15,7 +15,7 @@ context.lineWidth = 16;
 context.stroke();
 
 context.beginPath();
-context.moveTo(272, 136);
-context.lineTo(272, 286);
+context.moveTo(280, 144);
+context.lineTo(280, 294);
 context.lineWidth = 16;
 context.stroke();
