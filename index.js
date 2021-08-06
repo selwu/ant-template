71faa715d037b83c83882cf33cd4831ca3abf81e
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-08-06 22:48:53 +0300

    set to center

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const context = canvas.getContext('2d');
 context.strokeStyle = '#fff';
 
 context.beginPath();
-context.moveTo(50, 150);
-context.lineTo(50, 300);
+context.moveTo(128, 136);
+context.lineTo(128, 286);
 context.lineWidth = 16;
 context.stroke();
 
 context.beginPath();
-context.moveTo(72, 128);
-context.lineTo(172, 128);
+context.moveTo(150, 114);
+context.lineTo(250, 114);
 context.lineWidth = 16;
 context.stroke();
 
 context.beginPath();
-context.moveTo(194, 150);
-context.lineTo(194, 300);
+context.moveTo(272, 136);
+context.lineTo(272, 286);
 context.lineWidth = 16;
 context.stroke();
