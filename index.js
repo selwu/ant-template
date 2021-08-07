6a3a6b7b7c6ec3437b1a0705ae4fe942737502b2
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-08-07 11:59:18 +0300

    done move x y

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const left = document.addEventListener('keydown', leftHandler);
-const right = document.addEventListener('keydown', rightHandler);
-const up = document.addEventListener('keydown', upHandler);
-const down = document.addEventListener('keydown', downHandler);
+
 const MOVE_INT = 25;
+let xL = 120;
+let xLT = 150;
+let xLB = 250;
+let xLR = 280;
+let yL = 144;
+let yLL = 294;
+let yLT = 114;
 
 function leftHandler(event) {
   if (event.code === 'ArrowLeft') {
-    console.log('left');
+    xL -= MOVE_INT;
+    xLT -= MOVE_INT;
+    xLB -= MOVE_INT;
+    xLR -= MOVE_INT;
   }
 }
 
 function rightHandler(event) {
   if (event.code === 'ArrowRight') {
-    console.log('ArrowRight');
+    xL += MOVE_INT;
+    xLT += MOVE_INT;
+    xLB += MOVE_INT;
+    xLR += MOVE_INT;
   }
 }
 
 function upHandler(event) {
   if (event.code === 'ArrowUp') {
-    console.log('ArrowUp');
+    yL -= MOVE_INT;
+    yLL -= MOVE_INT;
+    yLT -= MOVE_INT;
   }
 }
 
 function downHandler(event) {
   if (event.code === 'ArrowDown') {
-    console.log('ArrowDown');
+    yL += MOVE_INT;
+    yLL += MOVE_INT;
+    yLT += MOVE_INT;
   }
 }
+
+const left = document.addEventListener('keydown', leftHandler);
+const right = document.addEventListener('keydown', rightHandler);
+const up = document.addEventListener('keydown', upHandler);
+const down = document.addEventListener('keydown', downHandler);
 
 canvas.width = 400;
 canvas.height = 400;
 context.strokeStyle = '#fff';
-
-const xL = 120;
-const yL = 144;
-const yLL = 294;
-const xLT = 150;
-const xLB = 250;
-const xLR = 280;
-const yLT = 114;
 
 requestAnimationFrame(tick);
 function tick() {
@@ -49,20 +60,20 @@ function tick() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 
   context.beginPath();
-  context.moveTo(120, 144);
-  context.lineTo(120, 294);
+  context.moveTo(xL, yL);
+  context.lineTo(xL, yLL);
   context.lineWidth = 16;
   context.stroke();
 
   context.beginPath();
-  context.moveTo(150, 114);
-  context.lineTo(250, 114);
+  context.moveTo(xLT, yLT);
+  context.lineTo(xLB, yLT);
   context.lineWidth = 16;
   context.stroke();
 
   context.beginPath();
-  context.moveTo(280, 144);
-  context.lineTo(280, 294);
+  context.moveTo(xLR, yL);
+  context.lineTo(xLR, yLL);
   context.lineWidth = 16;
   context.stroke();
 }
