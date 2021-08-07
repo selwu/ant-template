42c3a2d5059adacfa3ad2fd47fcf8b03e5fed5eb
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-08-07 11:42:31 +0300

    add animation

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+const left = document.addEventListener('keydown', leftHandler);
+const right = document.addEventListener('keydown', rightHandler);
+const up = document.addEventListener('keydown', upHandler);
+const down = document.addEventListener('keydown', downHandler);
+
+function leftHandler(event) {
+  if (event.code === 'ArrowLeft') {
+    console.log('left');
+  }
+}
+
+function rightHandler(event) {
+  if (event.code === 'ArrowRight') {
+    console.log('ArrowRight');
+  }
+}
+
+function upHandler(event) {
+  if (event.code === 'ArrowUp') {
+    console.log('ArrowUp');
+  }
+}
+
+function downHandler(event) {
+  if (event.code === 'ArrowDown') {
+    console.log('ArrowDown');
+  }
+}
+
+canvas.width = 400;
+canvas.height = 400;
 context.strokeStyle = '#fff';
 
-context.beginPath();
-context.moveTo(120, 144);
-context.lineTo(120, 294);
-context.lineWidth = 16;
-context.stroke();
+requestAnimationFrame(tick);
+function tick() {
+  requestAnimationFrame(tick);
 
-context.beginPath();
-context.moveTo(150, 114);
-context.lineTo(250, 114);
-context.lineWidth = 16;
-context.stroke();
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-context.beginPath();
-context.moveTo(280, 144);
-context.lineTo(280, 294);
-context.lineWidth = 16;
-context.stroke();
+  context.beginPath();
+  context.moveTo(120, 144);
+  context.lineTo(120, 294);
+  context.lineWidth = 16;
+  context.stroke();
+
+  context.beginPath();
+  context.moveTo(150, 114);
+  context.lineTo(250, 114);
+  context.lineWidth = 16;
+  context.stroke();
+
+  context.beginPath();
+  context.moveTo(280, 144);
+  context.lineTo(280, 294);
+  context.lineWidth = 16;
+  context.stroke();
+}
