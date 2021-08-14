64486db1b6f6663c23738aeeba99ffaa1e78eb28
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-08-14 20:01:21 +0300

    add parcel

--- index.js
+++ index.js
@@ -1,79 +1,219 @@
+/* Условия для логотипа:
+размер канваса — 400x400px;
+толщина линий — 16px;
+длина вертикальной линии — 150px;
+длина горизонтальной линии — 100px;
+цвет фона — чёрный;
+цвет линии — белый;
+расстояние по оси X от вертикальной до горизонтальной линии (ближайшие друг к другу границы) — 22px;
+расстояние по оси Y от вертикальной до горизонтальной линии — 22px;
+логотип должен располагаться по центру.
+Условия для анимации:
+	логотип передвигается по нажатию стрелок на клавиатуре: вверх, вниз, вправо, влево;
+	каждое передвижение смещает логотип целиком на 25px;
+	передвижение логотипа должно быть цикличным — если логотип выходит за пределы канваса, то исчезнувшая часть должна появиться с противоположной стороны.
+  
+Боковые поля от границ канваса должны быть по 112px.
+Вертикальные поля от границ канваса должны быть по 106px.
+*/
+
+import './style.css';
+
 const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+document.addEventListener('keydown', moveRect);
 
-const MOVE_INT = 25;
-let xL = 120;
-let xLT = 150;
-let xLB = 250;
-let xLR = 280;
-let yL = 144;
-let yLL = 294;
-let yLT = 114;
+const WIDTH = 400;
+const HEIGHT = 400;
 
-function leftHandler(event) {
-  if (event.code === 'ArrowLeft') {
-    xL -= MOVE_INT;
-    xLT -= MOVE_INT;
-    xLB -= MOVE_INT;
-    xLR -= MOVE_INT;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+ctx.fillStyle = '#fff';
+
+const HORIZONTAL_LINE_WIDTH = 100;
+const VERTICAL_LINE_HEIGHT = 150;
+const MARGIN = 22;
+const LINE_WIDTH = 16;
+const VERTICAL_OFFSET = 106;
+const HORIZONTAL_OFFSET = 112;
+const COMMON_OFFSET = MARGIN + LINE_WIDTH;
+
+const squares = [
+  // top squares
+  {
+    minX: HORIZONTAL_OFFSET + COMMON_OFFSET,
+    minY: VERTICAL_OFFSET,
+    width: 25,
+    height: LINE_WIDTH,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + COMMON_OFFSET + 25,
+    minY: VERTICAL_OFFSET,
+    width: 25,
+    height: LINE_WIDTH,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + COMMON_OFFSET + 25 * 2,
+    minY: VERTICAL_OFFSET,
+    width: 25,
+    height: LINE_WIDTH,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + COMMON_OFFSET + 25 * 3,
+    minY: VERTICAL_OFFSET,
+    width: 25,
+    height: LINE_WIDTH,
+  },
+  // left squares
+  {
+    minX: HORIZONTAL_OFFSET,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 2,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 3,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 4,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 5,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  // right squares
+  {
+    minX: HORIZONTAL_OFFSET + LINE_WIDTH + MARGIN * 2 + HORIZONTAL_LINE_WIDTH,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + LINE_WIDTH + MARGIN * 2 + HORIZONTAL_LINE_WIDTH,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + LINE_WIDTH + MARGIN * 2 + HORIZONTAL_LINE_WIDTH,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 2,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + LINE_WIDTH + MARGIN * 2 + HORIZONTAL_LINE_WIDTH,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 3,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + LINE_WIDTH + MARGIN * 2 + HORIZONTAL_LINE_WIDTH,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 4,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+  {
+    minX: HORIZONTAL_OFFSET + LINE_WIDTH + MARGIN * 2 + HORIZONTAL_LINE_WIDTH,
+    minY: VERTICAL_OFFSET + COMMON_OFFSET + 25 * 5,
+    width: LINE_WIDTH,
+    height: 25,
+  },
+];
+
+const init = () => updateRect(squares);
+
+// первая отрисовка фигур
+init();
+
+const clearRect = () => ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+function moveRect(event) {
+  console.log(event);
+  switch (event.keyCode) {
+    // left
+    case 37:
+      clearRect();
+
+      squares.forEach((square) => {
+        if (square.minX - 25 < 12) {
+          square.minX = 400 - 16;
+        } else {
+          square.minX -= 25;
+        }
+      });
+
+      updateRect(squares);
+      break;
+    // up
+    case 38:
+      clearRect();
+
+      squares.forEach((square) => {
+        if (square.minY - 25 < 12) {
+          square.minY = 400 - 25;
+        } else {
+          square.minY -= 25;
+        }
+      });
+
+      updateRect(squares);
+      break;
+    // right
+    case 39:
+      clearRect();
+
+      squares.forEach((square) => {
+        if (square.minX + 25 > 400 - 12) {
+          square.minX = 12;
+        } else {
+          square.minX += 25;
+        }
+      });
+
+      updateRect(squares);
+      break;
+    // down
+    case 40:
+      clearRect();
+
+      squares.forEach((square) => {
+        if (square.minY + 25 > 400) {
+          square.minY = 0;
+        } else {
+          square.minY += 25;
+        }
+      });
+
+      updateRect(squares);
+      break;
+    default:
+      break;
   }
 }
 
-function rightHandler(event) {
-  if (event.code === 'ArrowRight') {
-    xL += MOVE_INT;
-    xLT += MOVE_INT;
-    xLB += MOVE_INT;
-    xLR += MOVE_INT;
-  }
-}
-
-function upHandler(event) {
-  if (event.code === 'ArrowUp') {
-    yL -= MOVE_INT;
-    yLL -= MOVE_INT;
-    yLT -= MOVE_INT;
-  }
-}
-
-function downHandler(event) {
-  if (event.code === 'ArrowDown') {
-    yL += MOVE_INT;
-    yLL += MOVE_INT;
-    yLT += MOVE_INT;
-  }
-}
-
-const left = document.addEventListener('keydown', leftHandler);
-const right = document.addEventListener('keydown', rightHandler);
-const up = document.addEventListener('keydown', upHandler);
-const down = document.addEventListener('keydown', downHandler);
-
-canvas.width = 400;
-canvas.height = 400;
-context.strokeStyle = '#fff';
-
-requestAnimationFrame(tick);
-function tick() {
-  requestAnimationFrame(tick);
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  context.beginPath();
-  context.moveTo(xL, yL);
-  context.lineTo(xL, yLL);
-  context.lineWidth = 16;
-  context.stroke();
-
-  context.beginPath();
-  context.moveTo(xLT, yLT);
-  context.lineTo(xLB, yLT);
-  context.lineWidth = 16;
-  context.stroke();
-
-  context.beginPath();
-  context.moveTo(xLR, yL);
-  context.lineTo(xLR, yLL);
-  context.lineWidth = 16;
-  context.stroke();
+function updateRect(squares) {
+  //console.log(squares)
+  squares.forEach((square) => {
+    ctx.fillRect(square.minX, square.minY, square.width, square.height);
+  });
 }
